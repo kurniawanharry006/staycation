0907564dc18eab1d14b8565f348bad0f09d0c4ee
kurniawanharry006 <53112352+kurniawanharry006@users.noreply.github.com>
2020-12-23 15:51:23 +0700

    update hero

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import LandingPage from 'pages/LandingPage';
 import React from 'react'
+import LandingPage from 'pages/LandingPage';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
