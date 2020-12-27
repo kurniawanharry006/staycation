0645461f4eb93d613b0b8554e3e70c257449c3db
kurniawanharry006 <53112352+kurniawanharry006@users.noreply.github.com>
2020-12-27 21:09:42 +0700

    finish testimonial in landingPage

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -57,8 +57,8 @@ export default function Hero(props) {
                 </div>
                 <div className="col-6 pl-5">
                     <div style={{width:520,height:410}}>
-                        <img src={ImageHero} className="img-fluid position-absolute" style={{margin:'-30px 0 0 -30px',zIndex:1}} />
-                        <img src={ImageHero_} className="img-fluid position-absolute" style={{margin:'0 -15px 15px 0'}} />
+                        <img src={ImageHero}  alt="Room with couches" className="img-fluid position-absolute" style={{margin:'-30px 0 0 -30px',zIndex:1}} />
+                        <img src={ImageHero_} alt="Room with couches frame" className="img-fluid position-absolute" style={{margin:'0 -15px 15px 0'}} />
                     </div>
                 </div>
             </div>
